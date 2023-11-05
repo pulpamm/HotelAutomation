5acd1b553014799be76be386d3633852cc5d4e1e
Mustafa Ozbalci <mustafa.ozbalci@etiya.com>
2023-11-05 18:46:19 +0300

    first commit

---
+++ src/main/java/HotelAutomation/HotelAutomationApplication.java
@@ -0,0 +1,13 @@
+package HotelAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HotelAutomation/HotelAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package HotelAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
